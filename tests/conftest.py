b2d901037e0b9541e700632704c223dbf1dcda8d
x0pherl <xopher@interiority.org>
2024-12-29 11:30:26 -0500

    WIP: updates for testing first bit of documentation examples instead of arbitrary examples -- complete and idempotent, but more to come

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,19 @@
 import sys
 import os
+import pytest
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
 )
+
+
+@pytest.fixture
+def wheel_config_yaml():
+    return """
+wheel:
+    depth: 10
+    radius: 30
+    bearing:
+        radius: 4
+        spindle_radius: 1.5
+        """
---
+++ tests/test_partomatic_config.py
@@ -0,0 +1,133 @@
+from dataclasses import dataclass, field
+from enum import Enum, auto
+import pytest
+from unittest.mock import patch
+from pathlib import Path
+from partomatic import PartomaticConfig
+
+
+from build123d import BuildPart, Box, Sphere, Align, Mode, Location, Part
+
+
+class FakeEnum(Enum):
+    ONE = auto()
+    TWO = auto()
+    THREE = auto()
+
+
+class SubConfig(PartomaticConfig):
+    sub_field: str = "sub_default"
+    sub_enum: FakeEnum = FakeEnum.ONE
+
+
+class ContainerConfig(PartomaticConfig):
+    container_field: str = "container_default"
+    sub: SubConfig = field(default_factory=SubConfig)
+
+
+class BearingConfig(PartomaticConfig):
+    yaml_tree: str = "wheel/bearing"
+    radius: float = 10
+    spindle_radius: float = 2
+
+
+class WheelConfig(PartomaticConfig):
+    yaml_tree = "wheel"
+    depth: float = 2
+    radius: float = 50
+    bearing: BearingConfig = field(default_factory=BearingConfig)
+
+
+class TestPartomaticConfig:
+    config_yaml = """
+Part:
+    stl_folder: "yaml_folder"
+    file_prefix: "yaml_prefix"
+    file_suffix: "yaml_suffix"
+"""
+    blah_config_yaml = """
+Foo:
+    container_field: "yaml_container_field"
+    Blah:
+        stl_folder: "yaml_blah_folder"
+        file_prefix: "yaml_blah_prefix"
+        file_suffix: "yaml_blah_suffix"
+        sub_field: "yaml_sub_field"
+        sub_enum: "TWO"
+"""
+
+    sub_config_yaml = """
+Part:
+    container_field: "yaml_container_field"
+    sub:
+        stl_folder: "yaml_blah_folder"
+        file_prefix: "yaml_blah_prefix"
+        file_suffix: "yaml_blah_suffix"
+        sub_field: "yaml_sub_field"
+        sub_enum: "TWO"
+"""
+
+    def test_load_yaml_wheel(self, wheel_config_yaml):
+        config = WheelConfig(wheel_config_yaml)
+        assert config.depth == 10
+        assert config.radius == 30
+        assert config.bearing.__class__ == BearingConfig
+        assert config.bearing.radius == 4
+        assert config.bearing.spindle_radius == 1.5
+
+    def test_yaml_partomat(self):
+        config = PartomaticConfig(self.config_yaml)
+        assert config.stl_folder == "yaml_folder"
+
+    def test_empty_partomat(self):
+        config = PartomaticConfig()
+        assert config.stl_folder == "NONE"
+
+    def test_subconfig(self):
+        config = SubConfig(self.blah_config_yaml, yaml_tree="Foo/Blah")
+        assert config.stl_folder == "yaml_blah_folder"
+        assert config.sub_field == "yaml_sub_field"
+        assert config.sub_enum == FakeEnum.TWO
+
+    def test_kwargs(self):
+        config = SubConfig(yaml_tree="Part/Blah", sub_field="kwargsub")
+        assert config.stl_folder == "NONE"
+        assert config.sub_field == "kwargsub"
+
+    def test_yaml_container_partomat(self):
+        config = ContainerConfig(self.sub_config_yaml)
+        assert config.container_field == "yaml_container_field"
+        assert config.sub.sub_field == "yaml_sub_field"
+
+    def test_invalid_config(self):
+        with pytest.raises(ValueError):
+            ContainerConfig("invalid_config")
+
+    def test_yaml_container_with_dict_partomat(self):
+        config = ContainerConfig(
+            sub={
+                "stl_folder": "yaml_blah_folder",
+                "file_prefix": "yaml_blah_prefix",
+                "file_suffix": "yaml_blah_suffix",
+                "sub_field": "yaml_sub_field",
+                "sub_enum": "TWO",
+            }
+        )
+        assert config.sub.sub_field == "yaml_sub_field"
+
+    def test_yaml_container_with_class_partomat(self):
+        sub_config = SubConfig(self.blah_config_yaml, yaml_tree="Foo/Blah")
+
+        config = ContainerConfig(sub=sub_config)
+        assert config.sub.sub_field == "yaml_sub_field"
+
+    def test_default_container_partomat(self):
+        config = ContainerConfig()
+        assert config.container_field == "container_default"
+        assert config.sub.sub_field == "sub_default"
+
+    def test_config_create(self):
+        config = PartomaticConfig()
+        config.stl_folder = "config_create_folder"
+        config = PartomaticConfig(config)
+        assert config.stl_folder == "config_create_folder"
