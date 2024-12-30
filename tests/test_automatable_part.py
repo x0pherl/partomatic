58c7afdfc49466cd492838ad699c6aa9e9b0d4fa
x0pherl <xopher@interiority.org>
2024-12-30 16:17:58 -0500

    additions to test_automatable_part to ensure 100% test coverage

--- tests/test_automatable_part.py
+++ tests/test_automatable_part.py
@@ -14,7 +14,13 @@ class TestAutomatablePart:
         widget = AutomatablePart(widget_part, "widget.stl")
         assert widget.file_name == "widget"
 
-    def test_extension_removed(self):
+    def test_kwargs(self):
         widget_part = Part()
-        widget = AutomatablePart(widget_part, "widget.stl")
-        assert widget.file_name == "widget"
+        widget = AutomatablePart(
+            widget_part,
+            "widget.stl",
+            display_location=Location((100, 100, 100)),
+            stl_folder="/tmp/test/folder",
+        )
+        assert widget.display_location.position.X == 100
+        assert widget.stl_folder == "/tmp/test/folder"
