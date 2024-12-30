6738386d9220f0c449104745c2f1a7f28aa7b3a6
x0pherl <xopher@interiority.org>
2024-12-30 16:37:18 -0500

    partomatic tests updated to ensure complete coverage from test_partomatic.py and to cover example from documentation

--- tests/test_partomatic.py
+++ tests/test_partomatic.py
@@ -24,93 +24,6 @@ class ContainerConfig(PartomaticConfig):
     sub: SubConfig = field(default_factory=SubConfig)
 
 
-class TestPartomaticConfig:
-    config_yaml = """
-Part:
-    stl_folder: "yaml_folder"
-    file_prefix: "yaml_prefix"
-    file_suffix: "yaml_suffix"
-"""
-    blah_config_yaml = """
-Foo:
-    container_field: "yaml_container_field"
-    Blah:
-        stl_folder: "yaml_blah_folder"
-        file_prefix: "yaml_blah_prefix"
-        file_suffix: "yaml_blah_suffix"
-        sub_field: "yaml_sub_field"
-        sub_enum: "TWO"
-"""
-
-    sub_config_yaml = """
-Part:
-    container_field: "yaml_container_field"
-    sub:
-        stl_folder: "yaml_blah_folder"
-        file_prefix: "yaml_blah_prefix"
-        file_suffix: "yaml_blah_suffix"
-        sub_field: "yaml_sub_field"
-        sub_enum: "TWO"
-"""
-
-    def test_yaml_partomat(self):
-        config = PartomaticConfig(self.config_yaml)
-        assert config.stl_folder == "yaml_folder"
-
-    def test_empty_partomat(self):
-        config = PartomaticConfig()
-        assert config.stl_folder == "NONE"
-
-    def test_subconfig(self):
-        config = SubConfig(self.blah_config_yaml, yaml_tree="Foo/Blah")
-        assert config.stl_folder == "yaml_blah_folder"
-        assert config.sub_field == "yaml_sub_field"
-        assert config.sub_enum == FakeEnum.TWO
-
-    def test_kwargs(self):
-        config = SubConfig(yaml_tree="Part/Blah", sub_field="kwargsub")
-        assert config.stl_folder == "NONE"
-        assert config.sub_field == "kwargsub"
-
-    def test_yaml_container_partomat(self):
-        config = ContainerConfig(self.sub_config_yaml)
-        assert config.container_field == "yaml_container_field"
-        assert config.sub.sub_field == "yaml_sub_field"
-
-    def test_invalid_config(self):
-        with pytest.raises(ValueError):
-            ContainerConfig("invalid_config")
-
-    def test_yaml_container_with_dict_partomat(self):
-        config = ContainerConfig(
-            sub={
-                "stl_folder": "yaml_blah_folder",
-                "file_prefix": "yaml_blah_prefix",
-                "file_suffix": "yaml_blah_suffix",
-                "sub_field": "yaml_sub_field",
-                "sub_enum": "TWO",
-            }
-        )
-        assert config.sub.sub_field == "yaml_sub_field"
-
-    def test_yaml_container_with_class_partomat(self):
-        sub_config = SubConfig(self.blah_config_yaml, yaml_tree="Foo/Blah")
-
-        config = ContainerConfig(sub=sub_config)
-        assert config.sub.sub_field == "yaml_sub_field"
-
-    def test_default_container_partomat(self):
-        config = ContainerConfig()
-        assert config.container_field == "container_default"
-        assert config.sub.sub_field == "sub_default"
-
-    def test_config_create(self):
-        config = PartomaticConfig()
-        config.stl_folder = "config_create_folder"
-        config = PartomaticConfig(config)
-        assert config.stl_folder == "config_create_folder"
-
-
 @dataclass
 class WidgetConfig(PartomaticConfig):
     stl_folder: str = field(default="C:\\Users\\xopher\\Downloads")
@@ -122,8 +35,7 @@ class Widget(Partomatic):
 
     _config: WidgetConfig = WidgetConfig()
 
-    def compile(self):
-        self.parts.clear()
+    def complete_wheel(self) -> Part:
         with BuildPart() as holebox:
             Box(
                 self._config.length,
@@ -136,10 +48,14 @@ class Widget(Partomatic):
                 align=(Align.CENTER, Align.CENTER, Align.CENTER),
                 mode=Mode.SUBTRACT,
             )
+        return holebox.part
+
+    def compile(self):
+        self.parts.clear()
 
         self.parts.append(
             AutomatablePart(
-                holebox.part,
+                self.complete_wheel(),
                 "test",
                 display_location=Location((9, 0, 9)),
                 stl_folder=str(Path(self._config.stl_folder) / "stls"),
@@ -169,3 +85,18 @@ class TestPartomatic:
             foo.partomate()
         foo._config.stl_folder = "NONE"
         foo.export_stls()
+
+    def test_bad_stl_output_folder(self):
+        foo = Widget(stl_folder="/bad/path")
+        assert foo._config.stl_folder == "/bad/path"
+        with (
+            patch("pathlib.Path.mkdir"),
+            patch("pathlib.Path.exists", return_value=False),
+            patch("pathlib.Path.is_dir"),
+            patch("ocp_vscode.show"),
+            patch("build123d.export_stl"),
+            patch("build123d.export_step"),
+        ):
+            foo.display()
+            with pytest.raises(FileNotFoundError):
+                foo.partomate()
