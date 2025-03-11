f91b6a3e1b904d837dbdc4dd6fecb9461e86b499
x0pherl <xopher@interiority.org>
2025-03-10 20:47:36 -0400

    small fix to the build script

--- build.bat
+++ build.bat
@@ -24,7 +24,7 @@ py -m build
 py -m pip install -e .
 
 :TESTLOCAL
-python -c "exec(\"from fb_library import dovetail_subpart, click_fit, Point, shifted_midpoint\nprint(shifted_midpoint(Point(0,0), Point(10,10),0))\")"
+python -c "exec(\"from partomatic import Partomatic, PartomaticConfig, AutomatablePart\nprint(PartomaticConfig().stl_folder)\")"
 
 
 SET /P PYPI_UPLOAD=Based on that simple test, upload to pypi? ([Y]/N)?
